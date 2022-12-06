d330422d46b5c19dac7de04fa964f707cad36ad1
miguel Areiza <you@example.com>
2022-12-06 01:43:24 -0500

    clen

--- src/Config.js
+++ src/Config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports = {
     port: process.env.PORT || 4040,
-    DBuser: process.env.DBUSER || '',
-    DBpassword: process.env.DBPASS || '',
-    DBdatabase: process.env.DBDATABASE || '',
-    DBserver: process.env.DBSERVER || ''
+    DBuser: process.env.DBUSER || 'DannymateoH1_SQLLogin_1',
+    DBpassword: process.env.DBPASS || 'x5jcee19zb',
+    DBdatabase: process.env.DBDATABASE || 'MiguelDanny',
+    DBserver: process.env.DBSERVER || 'MiguelDanny.mssql.somee.com'
 };
